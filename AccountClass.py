6cd6579777e3c63cf6314035ce7551505fafe69a
matthewosborne71 <36384430+matthewosborne71@users.noreply.github.com>
2018-05-31 00:04:23 -0400

    Added Starter Code for Account Class
    We'll use this code for writing up our account class

---
+++ AccountClass.py
@@ -0,0 +1,8 @@
+import DataPath as DP
+
+class Account:
+    def __init__(self, username):
+        self.username = username
+    
+    def getTweet(CreatedTime):
+        print "This will be a function that pulls the specific tweet"
