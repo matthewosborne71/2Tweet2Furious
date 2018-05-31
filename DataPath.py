0b083e892107cf25dd01b6ad0fa4d4f5fd14003c
matthewosborne71 <36384430+matthewosborne71@users.noreply.github.com>
2018-05-31 14:56:14 -0400

    Delete DataPath.py

--- DataPath.py
+++
@@ -1,8 +0,0 @@
-## This file contains all of the path information for the 2Tweet2Furious
-## project
-
-def getPaths():
-    DataFramesPath = r'C:\\Users\\Matthew Osborne\\Documents\\python_code\\CodeTwitterBots\\'
-    TweetCSVsPath = r'C:\\Users\\Matthew Osborne\\Desktop\\Tweets\\'
-
-    return DataFramesPath, TweetCSVsPath
