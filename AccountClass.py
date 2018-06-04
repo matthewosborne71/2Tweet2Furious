34b6baa54f3cd181f7797a7e3e89ccb1a72160a4
matthewosborne71 <36384430+matthewosborne71@users.noreply.github.com>
2018-06-04 15:07:08 -0400

    Delete AccountClass.py

--- AccountClass.py
+++
@@ -1,8 +0,0 @@
-import DataPath as DP
-
-class Account:
-    def __init__(self, username):
-        self.username = username
-    
-    def getTweet(CreatedTime):
-        print "This will be a function that pulls the specific tweet"
